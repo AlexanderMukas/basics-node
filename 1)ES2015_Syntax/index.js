12e866a9ba057ca48110a86304207758d2dfdb9a
AlexanderMukas <alexandermukas@yahoo.com>
2020-12-04 00:20:44 +0200

    some more commit

--- 1)ES2015_Syntax/index.js
+++ 1)ES2015_Syntax/index.js
@@ -18,6 +18,35 @@ nums.forEach( (num, index) => {
 //block scoping with let
 for(let i = 0; i <= 4; ++i){
     setTimeout( () => {
-        console.log(i);
+        // console.log(i);
     }, 1000);
 }
+
+// default params
+function test1(a, b, c = 3) {
+    // console.log('default params:', a + b + c);
+};
+
+test1(1,2);
+
+// function variadics
+function sum(...nums) {
+    console.log(nums.reduce( (acc, a) => a + acc, 0));
+};
+
+sum(1, 5, 6);
+
+// spread operator
+console.log("// spread operator");
+let a = [1,2,3];
+let b = [...a, 4,5];
+console.log("let a = [1,2,3]; \n let b = [...a, 4,5]; \n" , b);
+
+
+// property shorthand
+var c = 'c';
+var d = 'd';
+
+// var e = {c: c, d: d};
+var e = { c, d };
+console.log(e);
