87fa8a14339457d108593c5562aa5fdc52ab1bc1
AlexanderMukas <alexandermukas@yahoo.com>
2020-12-14 23:51:59 +0200

    async first commit

---
+++ 3)Async_await/await_single.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const util = require('util');
+const read = util.promisify(fs.readFile);
+
+var run = async () => {
+    //promise version
+    read('text.txt')
+        .then( data => {
+            console.log( data.toString() )
+        });
+
+    // async/await version
+    const data = await read('text.txt')
+    console.log( data.toString() );
+};
+
+run();
